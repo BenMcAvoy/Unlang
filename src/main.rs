2b1665736400be6843bf6fd752e743dc9108b397
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-08 23:58:23 +0100

    Check file extension and switch to `minifemme`

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
-use kv_log_macro as log;
+// use kv_log_macro as log;
 
 use std::env;
 use std::fs;
 use std::path::Path;
 use std::process;
 
+fn usage() {
+    log::error!("Incorrect usage. Correct usage is:");
+    log::error!("unlang <input.ul>");
+
+    process::exit(-1);
+}
+
 fn main() {
-    femme::with_level(femme::LevelFilter::Trace);
+    minifemme::start(minifemme::LevelFilter::Trace, minifemme::LogMode::Pretty);
 
     let args: Vec<String> = env::args().map(|arg| arg.to_string()).collect();
 
     if args.len() != 2 {
-        log::error!("Incorrect usage. Correct usage is:");
-        log::error!("unlang <input.ul>");
-
-        process::exit(-1);
+        usage();
     }
 
     let path = Path::new(&args[1]);
+
+    if let Some(extension) = path.extension() {
+        if extension != "ul" {
+            log::error!("File must end in `.ul`");
+            usage();
+        }
+    }
+
     let contents = match fs::read_to_string(path) {
         Ok(v) => v,
         Err(e) => {
@@ -25,6 +37,8 @@ fn main() {
             process::exit(-1);
         }
     };
+
+    log::info!("Read file contents.");
 
     println!("{contents}");
 }

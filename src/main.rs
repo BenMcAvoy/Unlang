2b13f4a421c15872d6bdeb6430708bd6c3025db7
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-08 23:41:34 +0100

    Handle errors better

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use kv_log_macro as log;
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::process;
 
-fn main() -> Result<(), i32> {
+fn main() {
     femme::with_level(femme::LevelFilter::Trace);
 
     let args: Vec<String> = env::args().map(|arg| arg.to_string()).collect();
@@ -13,11 +14,17 @@ fn main() -> Result<(), i32> {
         log::error!("Incorrect usage. Correct usage is:");
         log::error!("unlang <input.ul>");
 
-        return Err(-1);
+        process::exit(-1);
     }
 
     let path = Path::new(&args[1]);
-    let contents = fs::read_to_string(path);
+    let contents = match fs::read_to_string(path) {
+        Ok(v) => v,
+        Err(e) => {
+            log::error!("Error reading file: {e}");
+            process::exit(-1);
+        }
+    };
 
-    Ok(())
+    println!("{contents}");
 }

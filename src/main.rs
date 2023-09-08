afa023fed776eaaf23e24000cf5e937d7a4e4384
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-09 00:06:50 +0100

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs;
 use std::path::Path;
 use std::process;
 
+/// Print out the program usage
 fn usage() {
     log::error!("Incorrect usage. Correct usage is:");
     log::error!("unlang <input.ul>");
@@ -13,16 +14,21 @@ fn usage() {
 }
 
 fn main() {
+    // Start logging
     minifemme::start(LevelFilter::Trace, LogMode::Pretty);
 
+    // Collect arguments
     let args: Vec<String> = env::args().map(|arg| arg.to_string()).collect();
 
+    // Check if right amount of arguments were provided
     if args.len() != 2 {
         usage();
     }
 
+    // Create a path from arguments
     let path = Path::new(&args[1]);
 
+    // Check if it ends in `ul`.
     if let Some(extension) = path.extension() {
         if extension != "ul" {
             log::error!("File must end in `.ul`");
@@ -30,6 +36,7 @@ fn main() {
         }
     }
 
+    // Read file contents
     let _contents = match fs::read_to_string(path) {
         Ok(v) => v,
         Err(e) => {

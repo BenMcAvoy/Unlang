ffb5a2ca50a2417760052470418f27cbf668856e
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-09 01:46:07 +0100

    Add code to tokenize the source code by stepping through

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,13 +1,20 @@
-// TODO: Convert to lexer struct!
+use std::process;
 
-pub enum TokenType {
+use crate::query::query;
+
+#[derive(Default, Debug)]
+pub enum TokenKind {
+    #[default]
+    Exit,
+
     Return,
     IntLit,
     Semi,
 }
 
+#[derive(Default, Debug)]
 pub struct Token {
-    kind: TokenType,
+    kind: TokenKind,
     value: Option<String>,
 }
 
@@ -23,20 +30,86 @@ impl Lexer {
         Self { source, index: 0 }
     }
 
-    pub fn tokenizer(&mut self) -> Vec<Token> {
-        todo!()
+    pub fn tokenize(&mut self) -> Vec<Token> {
+        let mut tokens: Vec<Token> = Vec::new();
+        let mut buf: Vec<char> = Vec::new();
+
+        while self.peek(1).is_some() {
+            if self.peek(1).unwrap().is_alphabetic() {
+                buf.push(self.consume());
+
+                while self.peek(1).unwrap().is_alphanumeric() {
+                    buf.push(self.consume());
+                }
+
+                let accumulated: String = buf.iter().map(|a| a.to_string()).collect();
+
+                log::info!("Accumulated: \"{accumulated}\"");
+
+                if &accumulated == "return" {
+                    // Exit token
+                    tokens.push(Token::default());
+
+                    buf.clear();
+                    continue;
+                } else {
+                    log::warn!("Accumulated: \"{accumulated}\"");
+                    log::error!("Temporary error. Something went wrong.");
+
+                    if query("Do you want to display a debug of the buffer?") {
+                        dbg!(&buf);
+                    }
+
+                    process::exit(-1);
+                }
+            } else if self.peek(1).unwrap().is_digit(10) {
+                buf.push(self.consume());
+
+                while self.peek(1).unwrap().is_digit(10) {
+                    buf.push(self.consume());
+                }
+
+                let accumulated: String = buf.iter().map(|a| a.to_string()).collect();
+
+                tokens.push(Token {
+                    kind: TokenKind::IntLit,
+                    value: Some(accumulated),
+                });
+                continue;
+            } else if self.peek(1).unwrap() == ';' {
+                tokens.push(Token {
+                    kind: TokenKind::Semi,
+                    value: None,
+                });
+                self.consume();
+                buf.clear();
+                continue;
+            } else if self.peek(1).unwrap().is_whitespace() {
+                self.consume();
+                continue;
+            } else {
+                log::error!("Temporary error. Something went wrong though.");
+                process::exit(-1);
+            }
+        }
+
+        // Reset incase used again.
+        self.index = 0;
+
+        tokens
     }
 
     fn peek(&self, ahead: i32) -> Option<char> {
         match self.index + ahead >= self.source.len() as i32 {
             true => return None,
-            false => return Some(self.source[self.index as usize])
+            false => return Some(self.source[self.index as usize]),
         }
     }
 
     fn consume(&mut self) -> char {
         self.index += 1;
 
-        return self.source[(self.index - 1) as usize]
+        // log::debug!("{}", self.source[(self.index - 1) as usize]);
+        return self.source[(self.index - 1) as usize];
     }
 }

a738b78d801f35d4df24aeba0d8caff60bcc7a87
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-09 21:59:58 +0100

    Create a generator to generate assembly

--- compiler/src/asm.rs
+++ compiler/src/asm.rs
@@ -1,28 +1,26 @@
-use crate::lexer::{Token, TokenKind};
+use crate::{lexer::{Token, TokenKind}, parser::node};
 use std::fmt::Write;
 
-pub fn tokens_to_asm(tokens: Vec<Token>) -> Result<String, Box<dyn std::error::Error>> {
-    let mut asm = String::new();
+pub struct Generator {
+    root: node::Exit,
+}
 
-    asm.push_str("global _start\n");
-    asm.push_str("_start:\n");
-
-    for (index, token) in tokens.iter().enumerate() {
-        if token.kind == TokenKind::Exit {
-            if tokens[index + 1].kind == TokenKind::IntLit {
-                log::debug!("IntLit");
-                if index + 1 < tokens.len() && tokens[index + 2].kind == TokenKind::Semi {
-                    log::debug!("Semi");
-
-                    let code = tokens[index + 1].value.clone().unwrap_or(String::from("1"));
-
-                    write!(asm, "    mov rax, 60\n")?;
-                    write!(asm, "    mov rdi, {}\n", code)?;
-                    write!(asm, "    syscall\n")?;
-                }
-            }
-        }
+impl Generator {
+    pub fn new(root: node::Exit) -> Self {
+        Self { root }
     }
 
-    Ok(asm)
+    pub fn generate(&self) -> Result<String, Box<dyn std::error::Error>> {
+        let mut asm = String::new();
+
+        asm.push_str("global _start\n");
+        asm.push_str("_start:\n");
+
+        // TODO: Don't use raw unwrap here.
+        write!(asm, "    mov rax, 60\n")?;
+        write!(asm, "    mov rdi, {}\n", self.root.expr.int_lit.value.unwrap())?;
+        write!(asm, "    syscall\n")?;
+
+        Ok(asm)
+    }
 }
